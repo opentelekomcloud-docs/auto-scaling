cfb5fc7343a59905d6c47c02e48d6cf6cb913424
Artem Goncharov <artem.goncharov@gmail.com>
2022-08-31 17:28:39 +0200

    Update conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- dev_guide/source/conf.py
+++ dev_guide/source/conf.py
@@ -80,7 +80,7 @@ html_theme_options = {
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 
-html_title = "Auto Scaling - User Guide"
+html_title = "Auto Scaling - Developer Guide"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -91,7 +91,7 @@ html_static_path = ['_static']
 # -- Options for PDF output --------------------------------------------------
 latex_documents = [
 ('index',
-     'as-umn.tex',
-     u'Auto Scaling - User Guide',
+     'as-dev-guide.tex',
+     u'Auto Scaling - Developer Guide',
      u'OpenTelekomCloud', 'manual'),
 ]
