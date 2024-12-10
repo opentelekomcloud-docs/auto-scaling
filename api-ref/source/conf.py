528ed3da3126edd554a1f36e3794d9833507e7ee
Sebastian Gode <sebastian.gode@telekom.de>
2024-12-10 10:15:47 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -110,7 +110,7 @@ html_copy_source = False
 latex_documents = [
     ('index',
      'as-api-ref.tex',
-     u'Auto Scaling - API Reference',
+    u'Auto Scaling - API Reference',
      u'OpenTelekomCloud', 'manual'),
 ]
 
@@ -125,12 +125,10 @@ latex_elements = {
   'pointsize': '12pt',
   'figure_align': 'H',
   'preamble': rf'''
-        
         \newcommand{{\githash}}{{{current_commit_hash}}}
         \newcommand{{\gitcommittime}}{{{current_commit_time}}}
         \newcommand{{\doctitle}}{{{otcdocs_doc_title}}}
         \newcommand{{\servicetitle}}{{{otcdocs_service_title}}}
-        
   ''',
   'sphinxsetup': 'hmargin={15mm,15mm}, vmargin={20mm,30mm}, marginpar=10mm'
 }
--- dev_guide/source/conf.py
+++ dev_guide/source/conf.py
@@ -109,8 +109,7 @@ html_copy_source = False
 # -- Options for PDF output --------------------------------------------------
 latex_documents = [
     ('index',
-     'as-dev-guide.tex',
-     u'Auto Scaling - Developer Guide',
+     'as-dev-guide.tex',u'Auto Scaling - Developer Guide',
      u'OpenTelekomCloud', 'manual'),
 ]
 
@@ -125,12 +124,10 @@ latex_elements = {
   'pointsize': '12pt',
   'figure_align': 'H',
   'preamble': rf'''
-        
         \newcommand{{\githash}}{{{current_commit_hash}}}
         \newcommand{{\gitcommittime}}{{{current_commit_time}}}
         \newcommand{{\doctitle}}{{{otcdocs_doc_title}}}
         \newcommand{{\servicetitle}}{{{otcdocs_service_title}}}
-        
   ''',
   'sphinxsetup': 'hmargin={15mm,15mm}, vmargin={20mm,30mm}, marginpar=10mm'
 }
